915b6e4731aa6f9d061a54e2516751b9e279cc47
bmyers4495 <bmyers4495@gmail.com>
2020-02-06 17:05:23 -0600

    added build for netlify

---
+++ build/precache-manifest.62cf6f106a3540e3538cd203869cc024.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c16789c4d6ac1ba24ab539c53a5afb26",
+    "url": "/testSite/index.html"
+  },
+  {
+    "revision": "dd9a0983722305a749ce",
+    "url": "/testSite/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "02b18b5004b8066b8488",
+    "url": "/testSite/static/css/main.ea9e79b7.chunk.css"
+  },
+  {
+    "revision": "dd9a0983722305a749ce",
+    "url": "/testSite/static/js/2.bcde8cc4.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/testSite/static/js/2.bcde8cc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02b18b5004b8066b8488",
+    "url": "/testSite/static/js/main.51fafe7c.chunk.js"
+  },
+  {
+    "revision": "a2ddc9f60f1e4c0557b8",
+    "url": "/testSite/static/js/runtime-main.e4286769.js"
+  },
+  {
+    "revision": "67ad7c37a8b67dfcc791b20f722b7b59",
+    "url": "/testSite/static/media/background_image-xsm.67ad7c37.jpg"
+  },
+  {
+    "revision": "0b02b058cbcededd9dbb216bf24aa6d3",
+    "url": "/testSite/static/media/favicon.0b02b058.ico"
+  }
+]);
